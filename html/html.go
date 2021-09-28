003648d83880b293c034b364657227a7e89550bd
Ivan Bragin <i.bragin@magdv.com>
2021-09-28 22:44:39 +0700

    html

--- html/html.go
+++ html/html.go
@@ -7,13 +7,13 @@ import (
 	"strings"
 )
 
-var reader *bufio.Reader = bufio.NewReader(os.Stdin)
-var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
-
-var stack *IntStack = NewStack()
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
 
 func printf(f string, a ...interface{}) { fmt.Fprintf(writer, f, a...) }
 func scanf(f string, a ...interface{})  { fmt.Fscanf(reader, f, a...) }
+
+var err error
 
 func main() {
 	defer writer.Flush()
@@ -37,37 +37,94 @@ type Tag struct {
 	Id      int
 }
 
+func (t Tag) same(tag Tag) bool {
+	return t.Value == tag.Value
+}
+
+func (t Tag) close(tag Tag) bool {
+	return t.Value == tag.Value && t.IsClose && ! tag.IsClose
+}
+
+type TagList []Tag
+
 func checkBlock() error {
+	err = nil
 	var n int
 	scanf("%d\n", &n)
 
 	list := readTags(n)
 
-	openIds, closeIds := isValid(list, -1)
-	if len(openIds) == 0 && len(closeIds) == 0 {
+	res := clearBlocks(list, 0, n)
+	if nil != err && nil == res {
+		return err
+	}
+	return res
+}
+
+func clearBlocks(list TagList, s int, e int) error {
+	if s >= e {
 		return nil
 	}
 
-	if len(openIds) == 0 && len(closeIds) == 1 {
-		return almostError(list[closeIds[0]])
-	}
+	top := list[s]
+	isTagClosed := false
+	var closeTag, cur Tag
 
-	if len(openIds) == 1 && len(closeIds) == 0 {
-		return almostError(list[openIds[0]])
-	}
+	if top.IsClose {
+		err = almostError(top)
+		s++
+	} else {
+		cnt := 1
+		for j := s + 1; j < e; j++ {
+			cur = list[j]
+			if ! cur.same(top) {
+				continue
+			}
 
-	for _, i := range openIds {
-		if o, c := isValid(list, i); len(o) == 0 && len(c) == 0 {
-			return almostError(list[i])
+			if cur.close(top) {
+				cnt --
+				if cnt == 0 {
+					if ! isTagClosed {
+						closeTag = cur
+					}
+					isTagClosed = true
+				} else if isTagClosed && cnt < 0 {
+					closeTag = cur
+					break
+				}
+			} else {
+				cnt ++
+			}
 		}
-	}
-	for _, i := range closeIds {
-		if o, c := isValid(list, i); len(o) == 0 && len(c) == 0 {
-			return almostError(list[i])
+
+		if isTagClosed {
+			err2 := clearBlocks(list, s+1, closeTag.Id)
+			if err != nil && err2 != nil {
+				return incorrectError()
+			}
+			if err2 != nil {
+				err = err2
+			}
+			s = closeTag.Id + 1
+		} else {
+			if err != nil {
+				return incorrectError()
+			}
+			err = almostError(top)
+			s++
 		}
+
 	}
 
-	return incorrectError()
+	err2 := clearBlocks(list, s, e)
+	if err != nil && err2 != nil {
+		return incorrectError()
+	}
+	if err2 != nil {
+		err = err2
+	}
+
+	return nil
 }
 
 func incorrectError() error {
@@ -80,44 +137,6 @@ func almostError(tag Tag) error {
 		slash = "/"
 	}
 	return fmt.Errorf("ALMOST <%s%s>", slash, tag.Value)
-}
-
-func isValid(list TagList, skipId int) ([]int, []int) {
-
-	openIds := make([]int, 0, len(list))
-	closeIds := make([]int, 0, len(list))
-
-	for _, tag := range list {
-		if tag.Id == skipId {
-			continue
-		}
-		if !tag.IsClose {
-			stack.push(tag.Id)
-			continue
-		}
-
-		if stack.length() > 0 {
-			for stack.length() > 0 {
-				topId := stack.pop()
-
-				if list[topId].Value == tag.Value {
-					break
-				}
-
-				openIds = append(openIds, topId)
-			}
-			continue
-		}
-
-		closeIds = append(closeIds, tag.Id)
-	}
-
-	for stack.length() > 0 {
-		tagId := stack.pop()
-		openIds = append(openIds, tagId)
-	}
-
-	return openIds, closeIds
 }
 
 func readTags(n int) TagList {
@@ -141,36 +160,4 @@ func readTags(n int) TagList {
 		}
 	}
 	return list
-}
-
-type IntStack struct {
-	ids []int
-}
-
-type TagList []Tag
-
-func NewStack() *IntStack {
-	return &IntStack{
-		ids: make([]int, 0),
-	}
-}
-
-func (s *IntStack) push(id int) {
-	s.ids = append(s.ids, id)
-}
-
-func (s *IntStack) getTop() int {
-	l := len(s.ids) - 1
-	return s.ids[l]
-}
-
-func (s *IntStack) pop() int {
-	l := len(s.ids) - 1
-	tag := s.ids[l]
-	s.ids = s.ids[:l]
-	return tag
-}
-
-func (s *IntStack) length() int {
-	return len(s.ids)
 }
